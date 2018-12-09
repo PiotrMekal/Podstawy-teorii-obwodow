fd103ee3c167e087b2346998c1906e98137353ac
PiotrMekal <piotrmekal4@gmail.com>
2018-12-09 23:28:45 +0100

    update

--- Podstawy-teorii-obwodow/Podstawy-teorii-obwodow/Form1.cs
+++ Podstawy-teorii-obwodow/Podstawy-teorii-obwodow/Form1.cs
@@ -16,5 +16,34 @@ namespace Podstawy_teorii_obwodow
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+           
+            Poziom1 poziom = new Poziom1();
+            poziom.Show();
+            Visible = false;
+           
+        
+        }
+
+        
+
+
     }
 }
