cc17d627ff8c1ee7d21a64d543578be2e6b98777
PiotrMekal <piotrmekal4@gmail.com>
2019-01-04 16:53:34 +0100

    update

--- Podstawy-teorii-obwodow/Podstawy-teorii-obwodow/Poziom1.Designer.cs
+++ Podstawy-teorii-obwodow/Podstawy-teorii-obwodow/Poziom1.Designer.cs
@@ -30,19 +30,28 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Poziom1));
             this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.r4 = new System.Windows.Forms.PictureBox();
+            this.r3 = new System.Windows.Forms.PictureBox();
+            this.r2 = new System.Windows.Forms.PictureBox();
+            this.r1 = new System.Windows.Forms.PictureBox();
+            this.r5 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.rysunek0 = new System.Windows.Forms.PictureBox();
+            this.dropR = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblwynik = new System.Windows.Forms.Label();
+            this.sprawdz = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.movObj = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.r4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.r3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.r2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.r1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.r5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rysunek0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dropR)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -54,50 +63,63 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Zad 1. Laczenie szeregowe rezystorow:";
             // 
-            // pictureBox1
+            // r4
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(474, 572);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(148, 145);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.r4.Image = ((System.Drawing.Image)(resources.GetObject("r4.Image")));
+            this.r4.Location = new System.Drawing.Point(474, 615);
+            this.r4.Name = "r4";
+            this.r4.Size = new System.Drawing.Size(100, 100);
+            this.r4.TabIndex = 1;
+            this.r4.TabStop = false;
+            this.r4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.r4_MouseClick);
+            this.r4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.r4_MouseDown);
             // 
-            // pictureBox2
+            // r3
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(320, 572);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(148, 145);
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
+            this.r3.Image = ((System.Drawing.Image)(resources.GetObject("r3.Image")));
+            this.r3.Location = new System.Drawing.Point(320, 615);
+            this.r3.Name = "r3";
+            this.r3.Size = new System.Drawing.Size(100, 100);
+            this.r3.TabIndex = 2;
+            this.r3.TabStop = false;
+            this.r3.Click += new System.EventHandler(this.r3_Click);
+            this.r3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.r3_MouseClick);
+            this.r3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.r3_MouseDown);
             // 
-            // pictureBox3
+            // r2
             // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(166, 572);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(148, 145);
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
+            this.r2.Image = ((System.Drawing.Image)(resources.GetObject("r2.Image")));
+            this.r2.Location = new System.Drawing.Point(166, 615);
+            this.r2.Name = "r2";
+            this.r2.Size = new System.Drawing.Size(100, 100);
+            this.r2.TabIndex = 3;
+            this.r2.TabStop = false;
+            this.r2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.r2_MouseClick);
+            this.r2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.r2_MouseDown);
             // 
-            // pictureBox4
+            // r1
             // 
-            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(12, 572);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(148, 145);
-            this.pictureBox4.TabIndex = 4;
-            this.pictureBox4.TabStop = false;
+            this.r1.BackColor = System.Drawing.SystemColors.Control;
+            this.r1.Image = ((System.Drawing.Image)(resources.GetObject("r1.Image")));
+            this.r1.Location = new System.Drawing.Point(12, 615);
+            this.r1.Name = "r1";
+            this.r1.Size = new System.Drawing.Size(100, 100);
+            this.r1.TabIndex = 4;
+            this.r1.TabStop = false;
+            this.r1.Click += new System.EventHandler(this.r1_Click);
+            this.r1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.r1_MouseClick);
+            this.r1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.r1_MouseDown);
             // 
-            // pictureBox5
+            // r5
             // 
-            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-            this.pictureBox5.Location = new System.Drawing.Point(628, 572);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(148, 145);
-            this.pictureBox5.TabIndex = 5;
-            this.pictureBox5.TabStop = false;
+            this.r5.Image = ((System.Drawing.Image)(resources.GetObject("r5.Image")));
+            this.r5.Location = new System.Drawing.Point(628, 615);
+            this.r5.Name = "r5";
+            this.r5.Size = new System.Drawing.Size(100, 100);
+            this.r5.TabIndex = 5;
+            this.r5.TabStop = false;
+            this.r5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.r5_MouseClick);
+            this.r5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.r5_MouseDown);
             // 
             // button1
             // 
@@ -132,28 +154,101 @@
             this.label2.Visible = false;
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
+            // rysunek0
+            // 
+            this.rysunek0.Image = ((System.Drawing.Image)(resources.GetObject("rysunek0.Image")));
+            this.rysunek0.Location = new System.Drawing.Point(417, 165);
+            this.rysunek0.Name = "rysunek0";
+            this.rysunek0.Size = new System.Drawing.Size(80, 401);
+            this.rysunek0.TabIndex = 9;
+            this.rysunek0.TabStop = false;
+            // 
+            // dropR
+            // 
+            this.dropR.Location = new System.Drawing.Point(397, 448);
+            this.dropR.Name = "dropR";
+            this.dropR.Size = new System.Drawing.Size(100, 100);
+            this.dropR.TabIndex = 10;
+            this.dropR.TabStop = false;
+            this.dropR.DragDrop += new System.Windows.Forms.DragEventHandler(this.dropR_DragDrop);
+            this.dropR.DragEnter += new System.Windows.Forms.DragEventHandler(this.dropR_DragEnter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(764, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Wynik: ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblwynik
+            // 
+            this.lblwynik.AutoSize = true;
+            this.lblwynik.Location = new System.Drawing.Point(804, 27);
+            this.lblwynik.Name = "lblwynik";
+            this.lblwynik.Size = new System.Drawing.Size(31, 13);
+            this.lblwynik.TabIndex = 12;
+            this.lblwynik.Text = "5000";
+            // 
+            // sprawdz
+            // 
+            this.sprawdz.Location = new System.Drawing.Point(760, 637);
+            this.sprawdz.Name = "sprawdz";
+            this.sprawdz.Size = new System.Drawing.Size(75, 23);
+            this.sprawdz.TabIndex = 13;
+            this.sprawdz.Text = "sprawdz";
+            this.sprawdz.UseVisualStyleBackColor = true;
+            this.sprawdz.Click += new System.EventHandler(this.sprawdz_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel1.Location = new System.Drawing.Point(528, 448);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(100, 100);
+            this.panel1.TabIndex = 14;
+            // 
+            // movObj
+            // 
+            this.movObj.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.movObj.Location = new System.Drawing.Point(693, 448);
+            this.movObj.Name = "movObj";
+            this.movObj.Size = new System.Drawing.Size(100, 100);
+            this.movObj.TabIndex = 15;
+            // 
             // Poziom1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.movObj);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.sprawdz);
+            this.Controls.Add(this.lblwynik);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dropR);
+            this.Controls.Add(this.rysunek0);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.r5);
+            this.Controls.Add(this.r1);
+            this.Controls.Add(this.r2);
+            this.Controls.Add(this.r3);
+            this.Controls.Add(this.r4);
             this.Controls.Add(this.label1);
             this.Name = "Poziom1";
             this.Text = "Poziom1";
             this.Load += new System.EventHandler(this.Poziom1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.r4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.r3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.r2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.r1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.r5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rysunek0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dropR)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -162,13 +257,20 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox r4;
+        private System.Windows.Forms.PictureBox r3;
+        private System.Windows.Forms.PictureBox r2;
+        private System.Windows.Forms.PictureBox r1;
+        private System.Windows.Forms.PictureBox r5;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox rysunek0;
+        private System.Windows.Forms.PictureBox dropR;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblwynik;
+        private System.Windows.Forms.Button sprawdz;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel movObj;
     }
 }
--- Podstawy-teorii-obwodow/Podstawy-teorii-obwodow/Poziom1.cs
+++ Podstawy-teorii-obwodow/Podstawy-teorii-obwodow/Poziom1.cs
@@ -15,11 +15,55 @@ namespace Podstawy_teorii_obwodow
         public Poziom1()
         {
             InitializeComponent();
+            r1.MouseDown += r1_MouseDown;
+            r2.MouseDown += r2_MouseDown;
+            r3.MouseDown += r3_MouseDown;
+            r4.MouseDown += r4_MouseDown;
+            r5.MouseDown += r5_MouseDown;
+            dropR.AllowDrop = true;
+            dropR.DragEnter += dropR_DragEnter;
+            dropR.DragDrop += dropR_DragDrop;
+            INIT();
+            
+        }
+
+
+
+        private Point firstPoint = new Point();
+
+        public void INIT()
+        {
+            movObj.MouseDown += (ss, ee) =>
+            {
+                if (ee.Button == System.Windows.Forms.MouseButtons.Left) { firstPoint = Control.MousePosition; }
+            };
+
+            movObj.MouseMove += (ss, ee) => {
+                if (ee.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    Point temp = Control.MousePosition;
+                    Point res = new Point(firstPoint.X - temp.X, firstPoint.Y - temp.Y);
+
+                    movObj.Location = new Point(movObj.Location.X - res.X, movObj.Location.Y - res.Y);
+
+                    firstPoint = temp;
+                }
+            };
+
+        }
+
+        public static class Wynik
+        {
+            public static float wynik=5000;
+            
+
         }
 
         private void Poziom1_Load(object sender, EventArgs e)
         {
-          
+            dropR.AllowDrop = true;
+            lblwynik.Text = Wynik.wynik.ToString();
+
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -33,6 +77,129 @@ namespace Podstawy_teorii_obwodow
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Poziom1 poziom = new Poziom1();
+            poziom.Show();
+            Visible = false;
+            
+        }
+
+        private void r1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void r2_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void r3_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void r4_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void r5_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void r1_MouseDown(object sender, MouseEventArgs e)
+        {           
+            var img = r1.Image;
+            if (img == null) return;
+            if (DoDragDrop(img, DragDropEffects.Move) == DragDropEffects.Move)
+            {
+                r1.Image = null;
+            }
+        }
+
+        private void r2_MouseDown(object sender, MouseEventArgs e)
+        {            
+            var img = r2.Image;
+            if (img == null) return;
+            if (DoDragDrop(img, DragDropEffects.Move) == DragDropEffects.Move)
+            {
+                r2.Image = null;
+            }
+        }
+
+        private void r3_MouseDown(object sender, MouseEventArgs e)
+        {           
+            var img = r3.Image;
+            if (img == null) return;
+            if (DoDragDrop(img, DragDropEffects.Move) == DragDropEffects.Move)
+            {
+                r3.Image = null;
+            }
+        }
+
+        private void r4_MouseDown(object sender, MouseEventArgs e)
+        {            
+            var img = r4.Image;
+            if (img == null) return;
+            if (DoDragDrop(img, DragDropEffects.Move) == DragDropEffects.Move)
+            {
+                r4.Image = null;
+            }
+        }
+
+        private void r5_MouseDown(object sender, MouseEventArgs e)
+        {           
+            var img = r5.Image;
+            if (img == null) return;
+            if (DoDragDrop(img, DragDropEffects.Move) == DragDropEffects.Move)
+            {
+                r5.Image = null;
+            }
+        }
+
+        private void dropR_DragEnter(object sender, DragEventArgs e)
+        {            
+            if (e.Data.GetDataPresent(DataFormats.Bitmap))
+                e.Effect = DragDropEffects.Move;
+        }
+
+        private void dropR_DragDrop(object sender, DragEventArgs e)
+        {
+            var bmp = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+            dropR.Image = bmp;
+            
+            
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sprawdz_Click(object sender, EventArgs e)
+        {
+            if (r3.Location.X != 320 && r3.Location.Y != 615)
+            {
+                button1.Visible = true;
+            }
+
+        }
+
+        private void r1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+        }
+
+        private void r3_Click(object sender, EventArgs e)
         {
 
         }
